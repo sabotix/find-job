809164c7538484f166d0bdddff84cbc377769727
Mustafa AKIN <sabotix@users.noreply.github.com>
2019-02-24 00:07:48 +0300

    Update settings.py

--- src/spring/settings.py
+++ src/spring/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q0kzzzc3tvmpjk*!6lm=6c6lw)ne+510a!-b_l-(!bl-jesil_'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
